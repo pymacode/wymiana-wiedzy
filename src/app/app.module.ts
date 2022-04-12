271b4d9b290bf17d9cc4428ecf2f2317ecbdd6bd
Maciej Pyszczak <maciej.pyszczak@envelo.pl>
2022-04-12 17:09:36 +0200

    add navigation component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { AppComponent } from './app.component';
 import { ShellComponent } from './shell/shell.component';
 import { BlankComponent } from './views/blank/blank.component';
+import { HeaderComponent } from './shell/header/header.component';
+import { NavigationComponent } from './navigation/navigation.component';
 const routes: Routes = [
   {
     path: 'app',
@@ -25,8 +29,19 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  declarations: [AppComponent, ShellComponent, BlankComponent],
-  imports: [BrowserModule, RouterModule.forRoot(routes)],
+  declarations: [
+    AppComponent,
+    ShellComponent,
+    BlankComponent,
+    HeaderComponent,
+    NavigationComponent,
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    MatIconModule,
+    MatButtonModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
