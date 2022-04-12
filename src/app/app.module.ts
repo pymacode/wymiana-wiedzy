6b5a3d5a370e5e8c785439fd753c92d9fdda7adc
Maciej Pyszczak <maciej.pyszczak@envelo.pl>
2022-04-12 15:35:07 +0200

    add navigation and blank page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ShellComponent } from './shell/shell.component';
+import { BlankComponent } from './views/blank/blank.component';
 const routes: Routes = [
   {
-    path: '',
+    path: 'app',
     component: ShellComponent,
     children: [
-      //Tutaj routing podrzędny
+      { path: 'add-workspace', component: BlankComponent },
+      {
+        path: 'workspace/:id',
+        component: BlankComponent,
+        children: [
+          { path: 'add-flashcard', component: BlankComponent },
+          { path: 'flashcard/:id', component: BlankComponent },
+        ],
+      },
     ],
   },
+  { path: '**', redirectTo: '/app' },
+  { path: '', pathMatch: 'full', redirectTo: 'app' },
 ];
 
 @NgModule({
-  declarations: [AppComponent, ShellComponent],
+  declarations: [AppComponent, ShellComponent, BlankComponent],
   imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent],
