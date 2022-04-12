b5f8d0ed47bf5b0ef42a59719406dd43edc9098c
Maciej Pyszczak <maciej.pyszczak@envelo.pl>
2022-04-12 11:21:09 +0200

    feat: Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { ShellComponent } from './shell/shell.component';
+const routes: Routes = [
+  {
+    path: '',
+    component: ShellComponent,
+    children: [
+      //Tutaj routing podrzędny
+    ],
+  },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ShellComponent],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
